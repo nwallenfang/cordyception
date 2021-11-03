5d60e0bece05925c2aabe0c673401a36f7609651
Nitai <nitai64@yahoo.de>
2021-11-03 13:37:49 +0100

    Aiming finished

--- Player/TopDownPlayer.gd
+++ Player/TopDownPlayer.gd
@@ -1,13 +1,12 @@
 extends SlideMover
 
 var aim_direction := 0.0
-onready var last_global_mouse := get_global_mouse_position()
-const AIM_THRESHHOLD := 0.1
+const MAX_MOUSE_DISTANCE := 50.0
 
 func _ready() -> void:
-	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	Input.set_custom_mouse_cursor(preload("res://Game Logic/invisible_cursor.png"))
 
 func get_input_vector() -> Vector2:
@@ -21,14 +20,15 @@ func _physics_process(delta: float) -> void:
 	var input_vec = get_input_vector()
 	accelerate_and_move(input_vec, delta)
 	$TestCursor.global_position = get_global_mouse_position()
-#	aim_direction = $Aimer.global_position.angle_to_point(get_global_mouse_position()) + Vector2.UP.angle()
-#	$Aimer.rotation = aim_direction
-	set_aimer(delta)
+	set_aimer()
 
-func set_aimer(delta: float) -> void:
+func set_aimer() -> void:
 	var global_mouse := get_global_mouse_position()
-	var mouse_diff := last_global_mouse - global_mouse
-	last_global_mouse = global_mouse
-	var mouse_movement := delta * mouse_diff.length()
-	if mouse_movement > 0:
-		$Aimer.rotation = mouse_diff.angle() + Vector2.UP.angle()
+	$Aimer.rotation = aim_direction
+	aim_direction = $Aimer.global_position.angle_to_point(global_mouse) + Vector2.UP.angle()
+	var distance_to_aimer := global_mouse.distance_to($Aimer.global_position)
+	if distance_to_aimer > MAX_MOUSE_DISTANCE:
+		var new_mouse := global_mouse.move_toward($Aimer.global_position, distance_to_aimer - MAX_MOUSE_DISTANCE)
+		get_viewport().warp_mouse(new_mouse)
+
+

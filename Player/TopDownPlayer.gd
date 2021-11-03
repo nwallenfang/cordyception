36a8473c77fec3dbded7c5774ea1f471ae59dd6c
Nitai <nitai64@yahoo.de>
2021-11-03 17:39:18 +0100

    Clean Up

--- Player/TopDownPlayer.gd
+++ Player/TopDownPlayer.gd
@@ -5,6 +5,7 @@ const PlayerProjectile := preload("res://Projectiles/Projectile.tscn")
 var aim_direction := 0.0
 
 var mouse_movement_input := Vector2.ZERO
+const MIN_TURN := PI / 20
 
 
 func _ready() -> void:
@@ -29,9 +30,9 @@ func _physics_process(delta: float) -> void:
 	handle_nonmovement_input()
 	accelerate_and_move(input_vec, delta)
 	
-	#update_mouse()
-	#update_aimer()
-	
+	update_mouse()
+
+func update_mouse():
 	var mouse_movement := mouse_movement_input
 	mouse_movement_input = Vector2.ZERO
 	
@@ -49,14 +50,13 @@ func _physics_process(delta: float) -> void:
 	var turn_sign := sign(turn)
 	var turn_abs := abs(turn)
 	
+	if turn_abs < MIN_TURN:
+		return
+	
 	var turn_done := clamp(speed, 0.0, turn_abs) * turn_sign
 	aim_direction += turn_done
 	
 	$Aimer.rotation = aim_direction
-	
-	# TEST CURSOR
-	#$TestCursor.global_position = get_global_mouse_position()
-	
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -70,32 +70,6 @@ func vector_to_angle(vec: Vector2) -> float:
 		vec = Vector2(0, -1)
 	vec.y = -vec.y
 	return vec.angle_to(Vector2.DOWN)
-
-#func move_mouse_to_local():
-#	var new_position := global_position + local_mouse_position
-#	if get_global_mouse_position().distance_to(new_position) < 2:
-#		return
-#	block_mouse_event = true
-#	get_viewport().warp_mouse(new_position)
-
-#func update_mouse() -> void:
-#	var global_mouse := get_global_mouse_position()
-#	var expected_global:Vector2 = global_position + local_mouse_position - get_last_movement()
-#	expected_global.x = int(expected_global.x)
-#	expected_global.y = int(expected_global.y)
-#	var difference := global_mouse - expected_global
-#	local_mouse_position += difference
-#	move_mouse_to_local()
-#
-#func update_aimer():
-#	var global_mouse := get_global_mouse_position()
-#	aim_direction = $Aimer.global_position.angle_to_point(global_mouse) + Vector2.UP.angle()
-#	$Aimer.rotation = aim_direction
-#	var distance_to_aimer := global_mouse.distance_to($Aimer.global_position)
-#	if distance_to_aimer > MAX_MOUSE_DISTANCE:
-#		var new_mouse := global_mouse.move_toward($Aimer.global_position, distance_to_aimer - MAX_MOUSE_DISTANCE)
-#		local_mouse_position += new_mouse - global_mouse
-#		move_mouse_to_local()
 
 func spawn_projectile() -> void: 
 	var projectile := PlayerProjectile.instance()

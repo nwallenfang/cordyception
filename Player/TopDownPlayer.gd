5b60d399634143280e74d6c9916f58b9266e82e1
Nitai <nitai64@yahoo.de>
2021-11-03 12:49:49 +0100

    Aiming Test Trash

--- Player/TopDownPlayer.gd
+++ Player/TopDownPlayer.gd
@@ -1,14 +1,14 @@
 extends SlideMover
 
+var aim_direction := 0.0
+onready var last_global_mouse := get_global_mouse_position()
+const AIM_THRESHHOLD := 0.1
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	Input.set_custom_mouse_cursor(preload("res://Game Logic/invisible_cursor.png"))
 
 func get_input_vector() -> Vector2:
 	var input_vector := Vector2.ZERO
@@ -20,3 +20,15 @@ func get_input_vector() -> Vector2:
 func _physics_process(delta: float) -> void:
 	var input_vec = get_input_vector()
 	accelerate_and_move(input_vec, delta)
+	$TestCursor.global_position = get_global_mouse_position()
+#	aim_direction = $Aimer.global_position.angle_to_point(get_global_mouse_position()) + Vector2.UP.angle()
+#	$Aimer.rotation = aim_direction
+	set_aimer(delta)
+
+func set_aimer(delta: float) -> void:
+	var global_mouse := get_global_mouse_position()
+	var mouse_diff := last_global_mouse - global_mouse
+	last_global_mouse = global_mouse
+	var mouse_movement := delta * mouse_diff.length()
+	if mouse_movement > 0:
+		$Aimer.rotation = mouse_diff.angle() + Vector2.UP.angle()

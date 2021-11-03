6a2b41177d9662e42a6af812dc1f67dbd14176cf
Nils Wallenfang <n.wallenfang@fz-juelich.de>
2021-11-03 22:49:39 +0100

    use projectile spawner

--- Projectiles/PlayerProjectileSpawner.gd
+++ Projectiles/PlayerProjectileSpawner.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+const PlayerProjectile := preload("res://Projectiles/Projectile.tscn")
+
 signal cooldown_not_ready
 
-#func create_
+func try_creating_projectile(direction: float) -> void:
+	if not $ProjectileCooldown.is_stopped(): # Timer still running
+		# Projectile shooting skill is not ready to fire again
+		emit_signal("cooldown_not_ready")  # TODO maybe add sound when this fires
+		return
+		
+	# create projectile
+	var projectile := PlayerProjectile.instance()
+	var main = get_tree().current_scene
+	main.add_child(projectile)
+	projectile.direction = Vector2.UP.rotated(direction)
+	projectile.global_position = self.global_position
+	
+	# start cooldown timer
+	$ProjectileCooldown.start()

b7b060aa330c45d730e7d55bcbfae2cb0a47e87e
Nils Wallenfang <n.wallenfang@fz-juelich.de>
2021-11-05 15:58:10 +0100

    add volley shot

--- Projectiles/EnemyProjectileSpawner.gd
+++ Projectiles/EnemyProjectileSpawner.gd
@@ -4,6 +4,16 @@ extends Node2D
 export var PROJECTILE_DAMAGE := 1
 const EnemyProjectile := preload("res://Projectiles/EnemyProjectile.tscn")
 
+export var SPAWN_TIME_MIN := 3
+export var SPAWN_TIME_MAX := 6
+
+var volley_index: int = 0
+
+func _ready() -> void:
+	randomize()
+	$DebugSpawnTimer.wait_time = rand_range(SPAWN_TIME_MIN, SPAWN_TIME_MAX)
+	$DebugSpawnTimer.start()
+
 func create_projectile(direction: float) -> void:	
 	# create projectile
 	var projectile := EnemyProjectile.instance()
@@ -17,5 +27,46 @@ func create_projectile(direction: float) -> void:
 
 
 func _on_SpawnTimer_timeout() -> void:
-	var random_direction = randf()*2*PI
-	create_projectile(random_direction)
+	if randi() % 2 == 1:
+		spawn_radial_projectiles(16)
+	else:
+		# TODO dirty code, only works if parent has Sprite node!
+		var direction: float = self.get_parent().get_node("Sprite").rotation
+		spawn_cone_projectile_volley(direction, 30, 5, 0.2, 3)
+		
+	$DebugSpawnTimer.wait_time = rand_range(SPAWN_TIME_MIN, SPAWN_TIME_MAX)
+
+
+func spawn_single_spread(base_direction:float, spread_in_degree:int, amount: int):
+	# helper method
+	# start with base direction turned half the spread 
+	var start_direction = base_direction - deg2rad(spread_in_degree) / 2
+	var step = deg2rad(spread_in_degree)/amount
+	
+	for i in range(amount):
+		create_projectile(start_direction + i * step)
+
+
+func spawn_cone_projectile_volley(base_direction:float, spread_in_degree:int, amount: int, time_delta:float, how_often:int):
+	# amount: how many projectiles per spread
+	# spread_in_degree: range of the projectiles per spread (say 45 degrees) half up/down
+	# base_direction: direction of center of spread
+	# time_delta: time between volley shots
+	# how_often: amount of volley shots
+	
+	$VolleyTimer.wait_time = time_delta
+	$VolleyTimer.start()
+	
+	for i in range(how_often):
+		spawn_single_spread(base_direction, spread_in_degree, amount)
+		yield($VolleyTimer, "timeout")
+
+	$VolleyTimer.stop()
+	 
+
+func spawn_radial_projectiles(amount: int) -> void:
+	var degree_step = 2*PI/amount	
+	var direction: float
+	
+	for i in range(amount):
+		create_projectile(i * degree_step)

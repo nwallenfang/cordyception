2fd38552c3f6fd7031bc56e47471a3d303af3b27
nwallenfang <nils@wallenfang.de>
2021-11-08 23:31:35 +0100

    in between state working on sprint

--- Enemies/AntEnemy.gd
+++ Enemies/AntEnemy.gd
@@ -5,19 +5,19 @@ enum State {
 	CHASE_PLAYER, IDLE, SPRINT, SHOOT_STUFF
 }
 
-export var IDLE_TIME := 0.5
-export var SPRINT_VELOCITY := 400
+export var IDLE_TIME := 1
 
 const idle_transition_chance = {
-	State.IDLE: 0.5,
+	State.IDLE: 0.0,
 	State.CHASE_PLAYER: 0.0,
-	State.SHOOT_STUFF: 0.1,
-	State.SPRINT: 0.4
+	State.SHOOT_STUFF: 0.05,
+	State.SPRINT: 0.95
 }
 
 # will be true for the first frame you are in a new state
 var first_time_entering = true
 var state = State.IDLE
+
 
 func _ready() -> void:
 	if OS.is_debug_build():
@@ -46,7 +46,9 @@ func _on_EnemyStats_health_changed() -> void:
 
 
 func _on_EnemyStats_health_zero() -> void:
-	queue_free()
+	# TODO I would like not to kick around dead bodies quite as hard..
+	set_velocity(Vector2.ZERO)
+	$AnimationPlayer.play("dying")  # queue_free is called at the end of this
 
 func match_state(delta):
 	$StateLabel.text = State.keys()[state]
@@ -64,32 +66,64 @@ func match_state(delta):
 	if this_was_the_first_time:
 		first_time_entering = false
 
+# variables local to state sprint, if another enemy needs this skill
+# you could take these variables and the method to a new script SprintAttack
+var distance_to_player
+var target_point
+const MAX_SPRINT_DISTANCE := 500
+export var SPRINT_VELOCITY := 400
+
+func begin_sprinting(delta: float) -> void:
+	var direction = $Line2D.points[1].angle() + PI/2
+	distance_to_player = ($Line2D.points[1] - $Line2D.points[0]).length()
+	
+	# 1. if this enemy is too far from the player, don't sprint
+	if distance_to_player > MAX_SPRINT_DISTANCE:
+		transition_to(State.IDLE)
+		return
+	
+	# 2. see if there is line of sight towards the player
+	# cast a ray between Enemy and player for this
+	var space_state = get_world_2d().direct_space_state
+	var ray_result = space_state.intersect_ray(self.global_position, GameStatus.CURRENT_PLAYER.global_position)
+	
+	if not ray_result.empty():
+		# there is some object between you and the player
+		# maybe this is not the time to go full on sprinting
+		transition_to(State.IDLE)
+		return
+	
+	# now we know there is direct line of sight and the player is close
+	# TODO wait for a while first and signify to the player that something
+	# gnarly is about to happen (TODO show attention mark above enemy)
+	# TODO maybe even play a slight sound
+	target_point = $Line2D.points[1] + position
+	
+	$SprintMovementTween.interpolate_property(self, "position", position, target_point, 1,
+	Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+	$SprintMovementTween.start()
+	
+	# TODO add soft collisions
+
 func state_sprint(delta):
 	if first_time_entering:
-		# see if there is line of sight towards the player
-		var direction = $Line2D.points[1].angle() + PI/2
+		begin_sprinting(delta)
+	else:
+		# execute the actual sprinting movement
+		# since we know there is clear line of sight there won't be collisions
+		# no reason to use Godot physics to simulate this movement
 		
-		# cast a ray between Enemy and player to see if there's line of sight
-		var space_state = get_world_2d().direct_space_state
-		var ray_result = space_state.intersect_ray(self.position, self.position + $Line2D.points[1])
-		
-		if ray_result != {}:
-			# there is some object between you and the player
-			# maybe this is not the time to go full on sprinting
-			transition_to(State.IDLE)
-			return
-		
-		# know we know there is direct line of sight
-		
-		self.move_and_slide(SPRINT_VELOCITY * direction)
-		
+		# we can try to use a tween to interpolate this movement for now
+		# the only question is whether collisions with the player mid-movement
+		# (and soft-collisions) work out of the box
+		pass
 		
 	
 func state_chase_player():
 	pass
 	
 func state_shoot_stuff():
-	if not first_time_entering:
+	if not first_time_entering:  # with the call_deferred this shouldn't be needed
 		return
 
 	# for now either shoot a cone in the player direction or shoot radial
@@ -104,7 +138,8 @@ func state_shoot_stuff():
 	# if too far from the player, don't shoot at all but enter another state instead
 	
 	# go back to being idle in the next frame
-	call_deferred("transition_to", State.IDLE)
+	transition_to(State.IDLE)
+
 	
 func transition_to(new_state):
 	first_time_entering = true
@@ -131,8 +166,9 @@ func transition_to_random_state():
 	
 	
 func state_idle():
-	if $StateTimer.is_stopped():
-		$StateTimer.start(IDLE_TIME)
+	if $IdleTimer.is_stopped():
+		$IdleTimer.start(1)
+	
 	
 
 func _physics_process(delta: float) -> void:
@@ -143,11 +179,6 @@ func _physics_process(delta: float) -> void:
 	match_state(delta)
 
 
-func _on_StateTimer_timeout() -> void:	
-	if state == State.IDLE:
-		transition_to_random_state()
-	else:
-		# this should not happen
-		transition_to(State.IDLE)
-	
-	$StateTimer.stop()
+func _on_IdleTimer_timeout() -> void:
+	transition_to_random_state()
+	$IdleTimer.stop()
--- Enemies/ScentRay.gd
+++ Enemies/ScentRay.gd
@@ -18,3 +18,22 @@ func get_player_scent_position() -> Vector2:
 				return scent.position
 	
 	return get_parent().position
+	
+	
+func get_player_scent_position_global() -> Vector2:
+	var player := GameStatus.CURRENT_PLAYER
+
+	cast_to = player.position - get_parent().position
+	force_raycast_update()
+	
+	if !is_colliding():
+		return player.global_position
+	else:
+		for scent in player.scent_spawner.scent_trail:
+			cast_to = scent.position - get_parent().position
+			force_raycast_update()
+
+			if !is_colliding():
+				return scent.global_position
+	
+	return get_parent().global_position

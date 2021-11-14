8878652865faeadfae01bd3f47716cf3403924f3
Nitai <nitai64@yahoo.de>
2021-11-14 20:34:53 +0100

    Cam Features

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -4,5 +4,10 @@ func _ready() -> void:
 	GameStatus.CURRENT_YSORT = $YSort
 	GameStatus.CURRENT_UI = $UI
 	GameStatus.CURRENT_PLAYER = $YSort/Player
+	GameStatus.CURRENT_CAMERA = $Camera2D
+	GameStatus.CURRENT_CAM_REMOTE = $YSort/Player/CamRemote
 	GameStatus.CURRENT_HEALTH = GameStatus.PLAYER_MAX_HEALTH
 	GameStatus.MOUSE_CAPTURE = true
+	
+	$Camera2D.follow($YSort/AphidPath/PathFollow2D/Aphid)
+	#$Camera2D.back_to_player()

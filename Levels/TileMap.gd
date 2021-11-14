193d682f56ef2a86c2959337ef6587b84d0456e1
Nitai <nitai64@yahoo.de>
2021-11-14 15:54:00 +0100

    Tile Map Changes

--- Levels/TileMap.gd
+++ Levels/TileMap.gd
@@ -1,7 +1,14 @@
 tool
 extends TileMap
 
-export var do_nothing := true
+export var do_nothing := true setget activate
+
+func activate(a: bool) -> void:
+	do_nothing = a
+	if !do_nothing:
+		fix_invalid_tiles()
+		update_dirty_quadrants()
+		update_bitmask_region()
 
 func _ready() -> void:
 	if !do_nothing:

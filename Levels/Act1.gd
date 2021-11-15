204762deb799a42206a1459476c677c364653dc3
nwallenfang <nils@wallenfang.de>
2021-11-15 19:22:43 +0100

    build act1

--- Levels/Act1.gd
+++ Levels/Act1.gd
@@ -8,3 +8,8 @@ func _ready() -> void:
 	GameStatus.CURRENT_CAM_REMOTE = $YSort/Player/CamRemote
 	GameStatus.CURRENT_HEALTH = GameStatus.PLAYER_MAX_HEALTH
 	GameStatus.MOUSE_CAPTURE = true
+
+
+# TutorialSpray Area2D
+func _on_Area2D_body_entered(body: Node) -> void:
+	GameStatus.SPRAY_ENABLED = true

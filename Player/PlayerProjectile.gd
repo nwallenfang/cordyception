a258cf4967ba6cf2eac26a81d2c2adc996d51178
nwallenfang <nils@wallenfang.de>
2021-11-20 14:36:28 +0100

    splash sound

--- Player/PlayerProjectile.gd
+++ Player/PlayerProjectile.gd
@@ -4,6 +4,9 @@ export var GREEN_LIGHT_ON := 1.8
 export var GREEN_LIGHT_OFF := 1.1
 
 
+func _ready() -> void:
+	$Hitbox/CollisionShape2D.disabled = false
+
 func turn_light_off():
 	var rgb_prev = $Sprite.modulate
 	$Sprite.modulate = Color(rgb_prev.r, GREEN_LIGHT_OFF, rgb_prev.b)

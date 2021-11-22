a31de1aeb2c0514e7ac9620ce8b81a424be723ee
nwallenfang <nils@wallenfang.de>
2021-11-22 18:08:19 +0100

    no splash sound outside of viewport

--- Player/PlayerProjectile.gd
+++ Player/PlayerProjectile.gd
@@ -4,6 +4,15 @@ export var GREEN_LIGHT_ON := 1.8
 export var GREEN_LIGHT_OFF := 1.1
 
 
+func splash_sound():  # maybe :)
+	# if projectile in view port (later addition TODO): or if it has hit an enemy
+	var transform: Transform2D = get_canvas_transform()
+	var scale: Vector2 = transform.get_scale()
+	var viewport_rect := Rect2(-transform.origin / scale, get_viewport_rect().size / scale)
+	
+	if viewport_rect.has_point(self.global_position):
+		$SplashAudio.play()
+
 func _ready() -> void:
 	$Hitbox/CollisionShape2D.disabled = false
 

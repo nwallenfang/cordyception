f5b054189ae3f0457275af07d2216ce581d602b0
nwallenfang <nils@wallenfang.de>
2021-11-23 22:42:06 +0100

    healthbar fadeout

--- UI/Healthbar.gd
+++ UI/Healthbar.gd
@@ -6,26 +6,48 @@ export var MAX_HEALTH: int = 200 setget set_max
 
 onready var health := MAX_HEALTH setget set_health
 
+var faded_out =  Color("00ffffff")
+var faded_in = Color("ffffffff")
+func _ready() -> void:
+	print(faded_out)
+	self.modulate = faded_out
+
 func set_max(new_max) -> void:
 	MAX_HEALTH = new_max
 	max_value = new_max
 	set_health(new_max)
 
+# TODO make invisible by default
 func set_health(new_health) -> void:
 	# health bar shouldn't be responsible for such logic checks but just to make
 	# sure..
 	new_health = min(new_health, MAX_HEALTH)
+	
+	if new_health < health:
+		self.modulate = faded_in
+		if $FadeTimer.is_stopped():
+			# reset timer
+			$FadeTimer.start()
+		print("fade cooldown")
+		$FadeTimer.start()
+	
 	health = new_health
 	self.value = health
-#	$Tween.interpolate_property(self, 'value', self.value, new_health, 0.4, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-
-func show():
-	# TODO
-	pass
 	
-func hide():
-	pass
+	
 
-
-func _ready() -> void:
-	pass
+	
+func _on_FadeTimer_timeout() -> void:
+	if $Tween.is_active():
+		print("fadeout already happening")
+		return
+	$FadeTween.reset_all()
+	$FadeTween.interpolate_property(self, "modulate", faded_in, faded_out, 1)
+	$FadeTween.start()
+	print("timeout")
+	
+	
+func _physics_process(delta: float) -> void:
+	print(modulate)
+	
+	
